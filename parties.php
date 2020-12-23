02b68c09fd8f54cb2e31c5bf53aecf0ddadefbf3
astropsumit <cosmictunnel376@gmail.com>
2020-12-24 00:47:50 +0530

    d

--- parties.php
+++ parties.php
@@ -1,56 +1,61 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-<title>nightclub | Parties</title>
-<meta charset="utf-8">
-<link rel="stylesheet" href="css/reset.css" type="text/css" media="all">
-<link rel="stylesheet" href="css/layout.css" type="text/css" media="all">
-<link rel="stylesheet" href="css/style.css" type="text/css" media="all">
-<script src="js/jquery-1.6.js" ></script>
-<script src="js/cufon-yui.js"></script>
-<script src="js/cufon-replace.js"></script>
-<script src="js/NewsGoth_BT_400.font.js"></script>
-<script src="js/NewsGoth_BT_700.font.js"></script>
-<script src="js/atooltip.jquery.js"></script>
-<!--[if lt IE 9]>
+  <title>nightclub | Parties</title>
+  <meta charset="utf-8">
+  <link rel="stylesheet" href="css/reset.css" type="text/css" media="all">
+  <link rel="stylesheet" href="css/layout.css" type="text/css" media="all">
+  <link rel="stylesheet" href="css/style.css" type="text/css" media="all">
+  <script src="js/jquery-1.6.js"></script>
+  <script src="js/cufon-yui.js"></script>
+  <script src="js/cufon-replace.js"></script>
+  <script src="js/NewsGoth_BT_400.font.js"></script>
+  <script src="js/NewsGoth_BT_700.font.js"></script>
+  <script src="js/atooltip.jquery.js"></script>
+  <!--[if lt IE 9]>
 <script src="js/html5.js"></script>
 <link rel="stylesheet" href="css/ie.css" type="text/css" media="all">
 <![endif]-->
 </head>
+
 <body id="page2">
-<div class="bg1">
-  <div class="main">
-    <!--header -->
-    <?php include $_SERVER['DOCUMENT_ROOT'] . '/components/header.php' ?>
-    <!--header end-->
-    <div class="box">
-      <!--content -->
-      <article id="content">
-        <div class="wrapper">
-          <h2>Upcoming Parties</h2>
+  <div class="bg1">
+    <div class="main">
+      <!--header -->
+      <?php include $_SERVER['DOCUMENT_ROOT'] . '/components/header.php' ?>
+      <!--header end-->
+      <div class="box">
+        <!--content -->
+        <article id="content">
           <div class="wrapper">
-            <div class="cols marg_right1">
-              <figure class="pad_bot1"><a href="#"><img src="images/page2_img1.jpg" alt=""></a></figure>
-              <figure><a href="#"><img src="images/page2_img2.jpg" alt=""></a></figure>
-            </div>
-            <div class="cols marg_right1">
-              <figure class="pad_bot1"><a href="#"><img src="images/page2_img3.jpg" alt=""></a></figure>
-              <figure><a href="#"><img src="images/page2_img4.jpg" alt=""></a></figure>
-            </div>
-            <div class="cols">
-              <figure class="pad_bot1"><a href="#"><img src="images/page2_img5.jpg" alt=""></a></figure>
-              <figure><a href="#"><img src="images/page2_img6.jpg" alt=""></a></figure>
+            <h2>Upcoming Parties</h2>
+            <div class="wrapper">
+              <div class="cols marg_right1">
+                <figure class="pad_bot1"><a href="#"><img src="images/page2_img1.jpg" alt=""></a></figure>
+                <figure><a href="#"><img src="images/page2_img1.jpg" alt=""></a></figure>
+              </div>
+              <div class="cols marg_right1">
+                <figure class="pad_bot1"><a href="#"><img src="images/page2_img1.jpg" alt=""></a></figure>
+                <figure><a href="#"><img src="images/page2_img1.jpg" alt=""></a></figure>
+              </div>
+              <div class="cols">
+                <figure class="pad_bot1"><a href="#"><img src="images/page2_img1.jpg" alt=""></a></figure>
+                <figure><a href="#"><img src="images/page2_img1.jpg" alt=""></a></figure>
+              </div>
             </div>
           </div>
-        </div>
-      </article>
-      <!--content end-->
-      <!--footer -->
-      <?php include $_SERVER['DOCUMENT_ROOT'] . '/components/footer.php' ?>
-      <!--footer end-->
+        </article>
+        <!--content end-->
+        <!--footer -->
+        <?php include $_SERVER['DOCUMENT_ROOT'] . '/components/footer.php' ?>
+        <!--footer end-->
+      </div>
     </div>
   </div>
-</div>
-<script>Cufon.now();</script>
+  <script>
+    Cufon.now();
+  </script>
 </body>
+
 </html>
